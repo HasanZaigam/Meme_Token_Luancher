d88f3c8301b49cc9a0c8fa32de9cc75dc14435fa
Hasan2k5 <syed.hasan.121415@gmail.com>
2025-03-14 03:34:45 +0530

    created the MyToken.sol contract which is the basic template

---
+++ contracts/MyToken.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20{
+   
+   address public owner;
+
+   constructor(string memory _name, string memory _symbol, uint256 _initialSupply) ERC20(_name, _symbol){
+    owner = msg.sender;
+    _mint(owner, _initialSupply * (10 ** decimals()));
+   }
+   
+   function mint(address to, uint256 amount) public {
+    require(msg.sender == owner, "Only owner can mint");
+    _mint(to, amount);
+   }
+   
+}
+   
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-ignition"); // ✅ Ignition Import Karo
+require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.19",
+  networks: {
+    hardhat: {},  // ✅ Hardhat local network bhi support karega
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,  // 🔗 Alchemy/Infura URL
+      accounts: [process.env.PRIVATE_KEY], // 🔑 Metamask Private Key
+    },
+  },
+  ignition: {
+    defaultNetwork: "hardhat",  // ✅ Default network Hardhat rakho
+  },
 };
